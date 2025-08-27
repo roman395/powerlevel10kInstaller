8047b1b08dafcef99db3267c14009f37ae04c0fa
roman395 <goruskinroma@gmail.com>
2025-08-27 17:36:51 +0300

    .zshrc

--- installer.py
+++ installer.py
@@ -1,48 +1,78 @@
 import subprocess as sub
 from os import geteuid
-"TODO:delete excess element"
+
+installCom: str
+isArch: bool
+
 distros = {
-'debian':'apt install',
-'rhel':'yum install',
-'arch':'pacman -S',
-'opensuse':'zypper install',
-'gentoo':'emerge',
-'termux':'pkg install'}
+    "debian": "apt install",
+    "rhel": "yum install",
+    "arch": "pacman -S",
+    "opensuse": "zypper install",
+    "gentoo": "emerge",
+}
 
-def init():
-  global installCom
-  global isArch
-"""
-  TODO: remove this comment
-  if geteuid != 0:
-    print('Exec with sudo')
-    exit(2)"""
-  print('Init start')
-  "TODO:change parh to normal"
-  out = sub.run('cat ~/*release | grep "^ID_LIKE="',stdout=sub.PIPE,encoding='utf-8',shell=True)
-  idLike = out.stdout.split('=', 1)[-1].strip()
 
-  if idLike == 'arch': isArch = True
-  else: isArch = False
+def init() -> None:
+    global installCom
+    global isArch
+    if geteuid() != 0:
+        print("Exec with sudo")
+        exit(2)
+    print("Init start")
+    # TODO:change parh to normal
+    out: sub.CompletedProcess = sub.run(
+        'cat /etc/*release | grep "^ID="',
+        stdout=sub.PIPE,
+        encoding="utf-8",
+        shell=True,
+    )
+    idLike = out.stdout.split("=", 1)[-1].strip()
 
-  try:
-    installCom = distros[idLike]
-    if input(f'Your install command is {installCom} \nIs it?(Y/n):') in ['y','Y','']:
-      print('ok')
+    if idLike == "arch":
+        isArch = True
     else:
-      installCom = input('Please input install comand(like: apt install, pacman -S, etc):')
-  except KeyError:
-     print('ERROR:PACKAGE MANAGER NOT FOUND! ABORDING')
-     exit(1)
-   print('Init complete') 
+        isArch = False
 
-def dependencies():
-  print('Installation start')
-  if !isArch:
-    sub.run(f'{installCom} -y zsh curl', shell=True)
-  else:
-    sub.run(f'{installCom} --noconfim zsh curl', shell=True)
-  sub.run('sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',shell=True)
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-  print('Installation complete')
-init()
+    try:
+        installCom = distros[idLike]
+        if input(f"Your install command is {installCom} \nIs it?(Y/n):") in [
+            "y",
+            "Y",
+            "",
+        ]:
+            print("ok")
+        else:
+            installCom = input(
+                "Please input install comand(like: apt install, pacman -S, etc):"
+            )
+    except KeyError:
+        print("ERROR:PACKAGE MANAGER NOT FOUND! ABORDING")
+        exit(1)
+    print("Init complete")
+
+
+def dependencies() -> None:
+    print("Installation start")
+    if not isArch:
+        sub.run(f"{installCom} -y zsh curl", shell=True)
+    else:
+        sub.run(f"{installCom} --noconfirm zsh curl", shell=True)
+    sub.run(
+        'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"',
+        shell=True,
+    )
+    sub.run(
+        'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"',
+        shell=True,
+    )
+    print("Installation complete")
+
+
+def main():
+    init()
+    dependencies()
+
+
+if __name__ == "__main__":
+    main()
